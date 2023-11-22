ad6b3c5c3987d58cffd865cbb16e39400740362f
bena26264 <149434980+bena26264@users.noreply.github.com>
2023-11-22 15:10:58 +0100

    api-android-modell

---
+++ app.py
@@ -0,0 +1,51 @@
+import pickle
+
+import numpy as np
+from flask import Flask, request, jsonify
+from waitress import serve
+#from ApiFlaskAndroid.wsgi import application
+
+model = pickle.load(open('model.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello word"
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    Src_bytes=request.form.get('Src_bytes')
+    Dst_bytes = request.form.get('Dst_bytes')
+    #Hot = request.form.get('Hot')
+    #Count = request.form.get('Count')
+    srv_count = request.form.get('srv_count')
+    diff_srv_rate = request.form.get('diff_srv_rate')
+    same_srv_rate = request.form.get('same_srv_rate')
+    #dst_host_srv_count = request.form.get('dst_host_srv_count')
+    #dst_host_same_srv_rate = request.form.get('dst_host_same_srv_rate')
+   # dst_host_diff_srv_rate = request.form.get('dst_host_diff_srv_rate')
+    dst_host_diff_srv_rate = request.form.get('dst_host_diff_srv_rate')
+    dst_host_srv_diff_host_rate = request.form.get('dst_host_srv_diff_host_rate')
+    dst_host_same_src_port_rate = request.form.get('dst_host_same_src_port_rate')
+    dst_host_serror_rate = request.form.get('dst_host_serror_rate')
+    dst_host_srv_serror_rate = request.form.get('dst_host_srv_serror_rate')
+    protocol_type = request.form.get('protocol_type')
+    Service = request.form.get('Service')
+    Flag = request.form.get('Flag')
+    DifficultyLevel = request.form.get('DifficultyLevel')
+
+    input_query = np.array([[Service,Flag,DifficultyLevel,protocol_type,Src_bytes,Dst_bytes,srv_count,diff_srv_rate,same_srv_rate,dst_host_diff_srv_rate
+                                ,dst_host_srv_diff_host_rate,dst_host_same_src_port_rate,dst_host_serror_rate,dst_host_srv_serror_rate]])
+    result = model.predict(input_query)[0]
+
+
+
+    return jsonify({'xAttack':str(result)})
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+#waitress-serve --listen=*:8000 myapp.wsgi:application
+
